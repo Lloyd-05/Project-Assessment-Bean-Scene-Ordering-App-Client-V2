46881d0868a8dcde1f635d4a6fa35df00afa49de
Lloyd-05 <lloyd.ryu@studytafensw.edu.au>
2026-07-11 23:36:08 +1000

    feat: add a deployed api to the client

--- src/router/APIEndpoint.js
+++ src/router/APIEndpoint.js
@@ -1,6 +1,5 @@
-const APIEndpoint="http://10.0.2.2:5000/api"
-
-
+// const APIEndpoint = "http://10.0.2.2:5000/api";
+const APIEndpoint = "https://project-assessment-bean-scene-ordering.onrender.com/api"
 // const APIEndpoint = "http://localhost:5000/api"; //This is running the client locally 
 
 
